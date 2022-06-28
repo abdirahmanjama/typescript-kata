944c359d7cc42d0ff3cb98a4c500b4ab0c423b2d
Abdirahman Jama <abdirahman.jama@thetrainline.com>
2022-06-28 17:40:45 +0100

    add ts-jest, add jest, add jest config, write tests

---
+++ src/contains-duplicates/index.test.ts
@@ -0,0 +1,19 @@
+import { containsDuplicate } from "./index";
+
+
+describe("contains duplicate", () => {
+    it("should return false if array is empty", () =>{    
+        const input : number[] = [];
+        expect(containsDuplicate(input)).toBe(false);
+    });
+
+    it("should return true if array contains duplicate", () =>{    
+        const input : number[] = [11,11,4,5,1,1];
+        expect(containsDuplicate(input)).toBe(false);
+    });
+
+    it("should return false if array contains no duplicate", () =>{    
+        const input : number[] = [11,9,4,5,1,1];
+        expect(containsDuplicate(input)).toBe(false);
+    });
+})
---
+++ src/contains-duplicates/index.ts
@@ -0,0 +1,3 @@
+export const containsDuplicate = (nums: number[]) : boolean => {
+    return nums.length > 0;
+}
