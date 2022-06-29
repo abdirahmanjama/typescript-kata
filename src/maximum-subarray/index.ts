aa151c24e224997d48c5d3321a4d8ef521e66c14
Abdirahman Jama <abdirahman.jama@thetrainline.com>
2022-06-29 08:54:47 +0100

    feat: complete kata with dynamic programming solution

--- src/maximum-subarray/index.ts
+++ src/maximum-subarray/index.ts
@@ -1,3 +1,12 @@
-export const getMaximumSubarray = (nums : number []) => {
-    return 0;
+export const getMaximumSubarray = (nums : number[]) => {
+    
+    let maxSubArray = nums[0];
+    let currentSubArray = nums[0];
+
+    for(let i = 1; i < nums.length; i++){
+        currentSubArray = Math.max(nums[i], currentSubArray + nums[i]);
+        maxSubArray = Math.max(currentSubArray, maxSubArray);
+    }
+
+    return maxSubArray;
 }
