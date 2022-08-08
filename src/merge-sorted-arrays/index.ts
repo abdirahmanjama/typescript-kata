55b55c375fb1053afc92679f843001d7648e7c6f
abdirahmanjama <abdirahman.jama@thetrainline.com>
2022-08-08 10:17:50 +0100

    fix typo

--- src/merge-sorted-arrays/index.ts
+++ src/merge-sorted-arrays/index.ts
@@ -1,4 +1,4 @@
 export const mergeSortedArray = (nums1 : number[], nums2 : number[], m : number, n: number) => {
-    nums1.splice(m, nums1.length, ...nums2);
+    nums1.splice(m, n, ...nums2);
     nums1.sort((a,b) => a-b);
 }
