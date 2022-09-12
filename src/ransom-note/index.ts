5528f78c8b26883a2b8321085f211c44eda23860
abdirahmanjama <abdirahman.jamaaa@gmail.com>
2022-09-12 06:11:30 +0100

    alter function

--- src/ransom-note/index.ts
+++ src/ransom-note/index.ts
@@ -1,3 +1,23 @@
-export const ransomNote = () => {
+export const ransomNote = (ransomNote: string, magazine: string) => {
+    let map: Record<string, number> = {}
+
+    for(let i = 0; i < magazine.length; i++){
+        if(map.hasOwnProperty(magazine.charAt(i))){
+            map[magazine[i]]--;
+        }
+    }
+    
+    for(let i = 0; i < ransomNote.length; i++){
+        if(!map.hasOwnProperty(ransomNote.charAt(i))){
+            map[ransomNote[i]] = 1;
+        }
+        else {
+            map[ransomNote[i]]++;
+        }
+    }
+
+   
+
+    return Object.values(map).reduce((prev, curr) => prev + curr, 0) === 0;
     
 }
