8a583955931684c3853fc9bac38b3b8f8e348554
abdirahmanjama <abdirahman.jamaaa@gmail.com>
2022-09-12 06:24:37 +0100

    complete exercise

--- src/ransom-note/index.ts
+++ src/ransom-note/index.ts
@@ -2,22 +2,50 @@ export const ransomNote = (ransomNote: string, magazine: string) => {
     let map: Record<string, number> = {}
 
     for(let i = 0; i < magazine.length; i++){
-        if(map.hasOwnProperty(magazine.charAt(i))){
-            map[magazine[i]]--;
+        if(!map.hasOwnProperty(magazine.charAt(i))){
+            map[magazine.charAt(i)] = 1;
+        }
+
+        else {
+            map[magazine.charAt(i)]++;
         }
     }
-    
+
     for(let i = 0; i < ransomNote.length; i++){
         if(!map.hasOwnProperty(ransomNote.charAt(i))){
-            map[ransomNote[i]] = 1;
+            return false;
+        }
+        if(map[ransomNote[i]] > 0){
+            map[ransomNote.charAt(i)]--;
+        }
+
+        else {return false}
+    }
+
+    return true;
+    
+}
+
+export const canConstruct = (ransomNote : string, magazine: string) => {
+    let map : Record<string, number> = {}
+
+    for(let char of magazine){
+        if(!map.hasOwnProperty(char)){
+            map[char] = 1;
         }
         else {
-            map[ransomNote[i]]++;
+            map[char]++;
         }
     }
 
-   
+    for(let char of ransomNote){
+        if(!map.hasOwnProperty(char)){
+            return false
+        }
+        else{
+            map[char]--;
+        }
+    }
 
-    return Object.values(map).reduce((prev, curr) => prev + curr, 0) === 0;
-    
+    return true;
 }
