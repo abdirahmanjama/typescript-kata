8d8f1cbd20563025294ba8e13069a15182760536
abdirahmanjama <abdirahman.jamaaa@gmail.com>
2022-09-12 07:00:58 +0100

    anagram function

--- src/valid-anagram/index.ts
+++ src/valid-anagram/index.ts
@@ -4,5 +4,26 @@
  * @param word - means erey in English
  */
 export const isAnagram = (erey : string, word: string ) => {
+    if(erey.length !== word.length) return false;
     let map : Record<string, number> = {}
+
+    for(const char of erey){
+        if(!map[char]){
+            map[char] = 1;
+        }
+        else{
+            map[char]++;
+        }
+    }
+
+    for(const char of word){
+        if(!map[char]){
+            return false;
+        }
+        else{
+            map[char]--;
+        }
+    }
+
+    return true;
 }
