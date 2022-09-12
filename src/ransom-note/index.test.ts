410e65ac16b7b27ec9c55388e70f97e73df7d66e
abdirahmanjama <abdirahman.jamaaa@gmail.com>
2022-09-12 06:55:53 +0100

    add docs for somali word

--- src/ransom-note/index.test.ts
+++ src/ransom-note/index.test.ts
@@ -1,6 +1,7 @@
 import { canConstruct } from ".";
 
 describe("canConstruct exercise", () => {
-    
+    it("should return false on non anagramatically values")
+    expect(canConstruct("a", "b")).toBe(false)
 })
 
--- src/valid-anagram/index.ts
+++ src/valid-anagram/index.ts
@@ -1,3 +1,8 @@
-export const isAnagram = (word : string) => {
-
+/**
+ * 
+ * @param erey - means word in Somali ;)
+ * @param word - means erey in English
+ */
+export const isAnagram = (erey : string, word: string ) => {
+    let map : Record<string, number> = {}
 }
