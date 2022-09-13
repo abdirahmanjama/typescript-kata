419aa0ddc1b3dfbe53cec3128420b8a2b5852479
abdirahmanjama <abdirahman.jamaaa@gmail.com>
2022-09-13 08:18:06 +0100

    finish exercise

--- src/palindrome/index.ts
+++ src/palindrome/index.ts
@@ -1,3 +1,10 @@
-export const isPalindrome = (word: string)  => {
 
+const alphanumeric = /^[0-9a-z]/gi;
+
+export const isPalindrome = (word: string)  => {
+    const term = word.toLowerCase().replace(alphanumeric, "");
+    for(let i = 0, j = term.length - 1; i <= j; i++, j--){
+        if(term[i] !== term[j]) return false
+    }
+    return true;
 }
