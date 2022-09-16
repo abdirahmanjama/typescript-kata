834839061376b9f8ea7ddf116d695ef36655512e
abdirahmanjama <abdirahman.jama@thetrainline.com>
2022-09-16 07:58:27 +0100

    start exercise

--- src/same-tree/index.ts
+++ src/same-tree/index.ts
@@ -1,10 +1,12 @@
-
 interface TreeNode {
     value: number;
     left: TreeNode | null;
     right: TreeNode | null;
 }
 
-export const isSameTree = (p: TreeNode, q: TreeNode) : Boolean => {
-    return false;
+export const isSameTree = (p: TreeNode | null, q: TreeNode | null) : Boolean => {
+    if(!p && !q) return true
+    if(!p || !q || p.value !== q.value) return false;
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, p.left);
 }
