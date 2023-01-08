f951908f54aa91329dcdb6d73ac4da585f0c5df7
abdirahmanjama <abdirahman.jama@thetrainline.com>
2023-01-08 16:38:53 +0000

    set up unit tests for exercise

---
+++ src/valid-paranthesis/index.test.ts
@@ -0,0 +1,11 @@
+import { isValid } from ".";
+
+describe("bracket matcher", () => {
+    test("returns true when given valid pairing", () => {
+
+    })
+
+    test("returns false when given invalid pairing", () => {
+
+    })
+})
