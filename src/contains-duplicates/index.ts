703c0cc7bc55edc1fa878052e671f8b407a89b95
abdirahmanjama <abdirahman.jama@thetrainline.com>
2023-01-08 16:29:07 +0000

    remove comments

--- src/contains-duplicates/index.ts
+++ src/contains-duplicates/index.ts
@@ -12,31 +12,3 @@ export const containsDuplicateTwin = (nums: number[]): boolean => {
 export const containsDuplicate = (nums: number[]): boolean => {
     return new Set(nums).size !== nums.length;
 }
-
-
-/**
- * Contains duplicate explained (3 possible solutions):
- * 
- * In this exercise, we want to write a function that determines whether a list or collection has any duplicates. 
- * 
- * Solution 1: The brute force approach would be to compare each value with every other value in the list and check to see if 
- * the values are the same. (this is what geezy did)
- * 
- * 
- * 
- * Solution 2: Another approach would be to simply create a new Set and pass in the values from the array.  (we do this in second function)
- * If the size of the set doesn't equal the original, then it means we must've had duplicates 
- * This is because sets cant have duplicates
- * 
- * 
- * 
- * Solution 3: Another approach would be to create a hashmap that looks like (we do this in first function)
- * 
- *    {
- *      value: counter 
- *    }
- * 
- * We can iterate through the list and add each element to the map, 
- * if a key already exists then it means we've already seen that value so we can return true. 
- * Else return false if we've exhaused all the elemtns
- */
