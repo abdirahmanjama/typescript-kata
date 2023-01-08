c070fcc521bc91cf2746a3b1fd3c2740cc936767
abdirahmanjama <abdirahman.jama@thetrainline.com>
2023-01-08 16:28:52 +0000

    remove comments

--- src/contains-duplicates/index.ts
+++ src/contains-duplicates/index.ts
@@ -15,22 +15,28 @@ export const containsDuplicate = (nums: number[]): boolean => {
 
 
 /**
- * Contains duplicate explained:
+ * Contains duplicate explained (3 possible solutions):
  * 
- * In this exercise, we want to write a function that determines whether a list or collection has any duplicates
+ * In this exercise, we want to write a function that determines whether a list or collection has any duplicates. 
  * 
- * The brute force approach would be to compare each value with every other value in the list and check to see if the values are the same. (this is what geezy did)
+ * Solution 1: The brute force approach would be to compare each value with every other value in the list and check to see if 
+ * the values are the same. (this is what geezy did)
  * 
- * Another approach would be to simply create a new Set and pass in the values from the array.  (we do this in second function)
+ * 
+ * 
+ * Solution 2: Another approach would be to simply create a new Set and pass in the values from the array.  (we do this in second function)
  * If the size of the set doesn't equal the original, then it means we must've had duplicates 
  * This is because sets cant have duplicates
  * 
  * 
- * Another approach would be to create a hashmap that looks like (we do this in first function)
+ * 
+ * Solution 3: Another approach would be to create a hashmap that looks like (we do this in first function)
  * 
  *    {
- *      value: counter  (in this hashmap, we make the key of the map the value in the list e.g. the number, the actual value of the key would be the counter)
+ *      value: counter 
  *    }
  * 
- * We can iterate through the list and add each element to the map, if a key already exists then it means we've already seen that value so we can return true. Else return false if we've exhaused all the elemtns
+ * We can iterate through the list and add each element to the map, 
+ * if a key already exists then it means we've already seen that value so we can return true. 
+ * Else return false if we've exhaused all the elemtns
  */
