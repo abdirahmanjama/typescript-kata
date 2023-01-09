d083b53fda0fe6006cf8293b57f075ffd13783cd
abdirahmanjama <abdirahman.jama@thetrainline.com>
2023-01-09 06:43:47 +0000

    second implm

--- src/valid-paranthesis/index.ts
+++ src/valid-paranthesis/index.ts
@@ -1,7 +1,24 @@
 
 
 //reminder: given string with '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-export const isValid = (paranthesis: string) => {
-    return true
+export const isValid = (parenthesis: string) => {
+    const map: any = {
+        "{": "}",
+        "[": "]",
+        "(": ")"
+    }
+
+    const stack: any = [];
+
+    for (const bracket of parenthesis) {
+        if (bracket in map) {
+            stack.push(bracket)
+        }
+        else if (map[stack.pop()] !== bracket) {
+            return false
+        }
+    }
+
+    return stack.length === 0;
 }
 
