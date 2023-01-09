a2065e63e090008d69ced548af4b15aed8664530
abdirahmanjama <abdirahman.jama@thetrainline.com>
2023-01-09 06:50:38 +0000

    add tests for valid parens

--- src/valid-paranthesis/index.test.ts
+++ src/valid-paranthesis/index.test.ts
@@ -2,10 +2,12 @@ import { isValid } from ".";
 
 describe("bracket matcher", () => {
     test("returns true when given valid pairing", () => {
-
+        const brackets = "[[]]"
+        expect(isValid(brackets)).toBe(true);
     })
 
     test("returns false when given invalid pairing", () => {
-
+        const brackets = "[[]"
+        expect(isValid(brackets)).toBe(false);
     })
 })
