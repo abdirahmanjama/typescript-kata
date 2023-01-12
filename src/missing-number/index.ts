d844a0f39ce1c5968a34d7a1e65bd837f1b6cbad
abdirahmanjama <abdirahman.jama@thetrainline.com>
2023-01-12 22:52:46 +0000

    add plans

--- src/missing-number/index.ts
+++ src/missing-number/index.ts
@@ -1,3 +1,7 @@
-export const missingNumber = () => {
+export const missingNumber = (numbers: number[]) => {
+    //first approach: sort array, loop through, if i != num[i], return i. 
+
+    //second approach use gauss elimination, calculate expected total, then calculate actual total, return expected - actual. 
+
 
 }
